b2040fbe679abe898284c766624a65574477deaa
dveloper-Elisa <kwizeraelisa77@gmail.com>
2024-11-14 15:21:58 -0500

    --noedit

--- src/pages/animationstionVideo.jsx
+++ src/pages/animationstionVideo.jsx
@@ -10,7 +10,7 @@ const AnimationsDt = () => {
             <h2 className="text-center py-10 font-bold font-mono text-3xl sm:text-3xl md:text-3xl bg-zinc-800 my-5 text-white break-all">Animations</h2>
             <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-3">
                 {animationData.map((pro, index) => (
-                    <Videos key={index} src={pro.src} title={pro.title} />
+                    <Videos key={index} src={pro.src} title={pro.title} className="w-[100%]"/>
                 ))}
             </div>
             </div>
--- src/pages/workdouts.jsx
+++ src/pages/workdouts.jsx
@@ -14,7 +14,7 @@ const WorkoutProject = () => {
           Projects
         </h2>
 
-        <div className="w-full grid grid-cols-1 items-center sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-3 transition duration-300">
+        <div className="w-full grid grid-cols-1 items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 transition duration-300">
           {projects.map((pro, index) => (
             <Project
               key={index}
